e5d3ec47de0c350713cd555290359a6374cbe7dc
Ahmed Sigiuk <ahmed.sigiuk@gmail.com>
2021-10-05 01:41:43 -0700

    temp

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 if __name__=="__main__":
 
     import subprocess
-    subprocess.run("mvNCCompile -s 12 -o test_3  optimized.pb  -in=input -on=MobilenetV1/Predictions/Reshape_1".split())
+    import os
+    my_env = os.environ.copy()
+    print(my_env)
+    my_env["PYTHONHOME"] = "/opt/movidius/virtualenv-python/bin/python"
+    my_env["PATH"] = "/opt/movidius/virtualenv-python/bin/python"
+    command = "mvNCCompile -s 12 -o test_3  optimized.pb  -in=input -on=MobilenetV1/Predictions/Reshape_1"
+    subprocess.run(.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=my_env)
